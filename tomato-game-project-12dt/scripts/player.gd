ef7fe50749f67fb16850a96d3770ad1630f2f89f
sli-svg <sli@students.mags.school.nz>
2026-06-07 22:36:59 +1200

    Player Movement Code Added

--- tomato-game-project-12dt/scripts/player.gd
+++ tomato-game-project-12dt/scripts/player.gd
@@ -1,43 +1,17 @@
 extends CharacterBody2D
 
-var movespeed = 500
+var speed: float = 300.0
 
-
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
 
 func _ready():
 	pass
 
 
-func _physics_process(delta):
-	var motion = Vector2()
+func _process(delta: float) -> void:
+	var direction: Vector2 = Vector2(0.0, 0.0)
+	direction.x = Input.get_axis("ui_left", "ui_right")
+	direction.y = Input.get_axis("ui_up", "ui_down")
 	
-	if Input.is_action_just_pressed('up'):
-		motion.y -= 1
-	if Input.is_action_just_pressed('down'):
-		motion.y += 1
-	if Input.is_action_just_pressed('up'):
-		motion.x -= 1
-	if Input.is_action_just_pressed('up'):
-		motion.x += 1
+	velocity = speed * direction.normalized()
 	
-	motion = motion.normalized()
-	
-	motion = move_and_slide(motion * movespeed)
-
-
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
 	move_and_slide()
